82db15fd0d45afb0bd6073b7ea5c413e3de0d45b
Olena Kalitsinska <olena.kalitsinka@gmail.com>
2024-08-09 10:02:44 +0300

    fixed flake8 single quotes

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -6,13 +6,13 @@ from cinema.models import Movie, Genre, CinemaHall, Actor, MovieSession
 class ActorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Actor
-        fields = ['id', 'first_name', 'last_name', 'full_name']
+        fields = ["id", "first_name", "last_name", "full_name"]
 
 
 class CinemaHallSerializer(serializers.ModelSerializer):
     class Meta:
         model = CinemaHall
-        fields = ['id', 'name', 'rows', 'seats_in_row', 'capacity']
+        fields = ["id", "name", "rows", "seats_in_row", "capacity"]
 
 
 class GenreSerializer(serializers.ModelSerializer):
